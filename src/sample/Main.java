bdb60761fb5af55fb31c72a48752d6ba725a37be
Jeremy-michiels <19014902@student.hhs.nl>
2020-07-03 18:57:17 +0200

    .

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,25 +9,36 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
-    Button een = new Button("Knop 1");
-    Button twee = new Button("Knop 2");
-    Button drie = new Button("Knop 3");
+    private static final BorderPane Layout = new BorderPane();
+    private static final Button een = new Button("Knop 1");
+    private static final Button twee = new Button("Knop 2");
+    private static final Button drie = new Button("Knop 3");
 
-    Button vier = new Button("Knop 4");
-    Button vijf = new Button("Knop 5");
-    Button zes = new Button("Knop 6");
+    private static Button vier = new Button("Knop 4");
+    private static Button vijf = new Button("Knop 5");
+    private static Button zes = new Button("Knop 6");
 
-    Button zeven = new Button("Knop 7");
-    Button acht = new Button("Knop 8");
-    Button negen = new Button("Knop 9");
+    private static Button zeven = new Button("Knop 7");
+    private static Button acht = new Button("Knop 8");
+    private static Button negen = new Button("Knop 9");
 
-    VBox rij1 = new VBox();
-    VBox rij2 = new VBox();
-    VBox rij3 = new VBox();
+    private static VBox rij1 = new VBox();
+    private static VBox rij2 = new VBox();
+    private static VBox rij3 = new VBox();
 
     HBox last = new HBox();
+    private static Stage window;
+
+    private static void initiate() {
+        Scene scn = new Scene(Layout, 800, 600);
+        scn.getStylesheets().add(Main.class.getResource("../css/Style.css").toExternalForm());
+        window.setScene(scn);
+        window.setMaximized(false);
+        window.show();
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
