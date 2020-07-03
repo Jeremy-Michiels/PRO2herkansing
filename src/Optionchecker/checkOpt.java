699c55db9d944e0af4238e1c4dda56ca337cdcfd
Jeremy-michiels <19014902@student.hhs.nl>
2020-07-03 20:20:10 +0200

    Further development

---
+++ src/Optionchecker/checkOpt.java
@@ -0,0 +1,24 @@
+package Optionchecker;
+
+import javafx.scene.control.Button;
+
+public class checkOpt {
+    public static String opt(Button een, Button twee, Button drie) {
+        if (een.getStyleClass().contains("X")) {
+            if (twee.getStyleClass().contains("X")) {
+                if (drie.getStyleClass().contains("X")) {
+                    return "X Wins";
+                }
+            }
+        }
+        if (een.getStyleClass().contains("O")) {
+            if (twee.getStyleClass().contains("O")) {
+                if (drie.getStyleClass().contains("O")) {
+                    return "O Wins";
+                }
+            }
+        }
+
+        return null;
+    }
+}
