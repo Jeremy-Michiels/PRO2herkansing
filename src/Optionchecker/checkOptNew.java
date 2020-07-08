b3527f1fe6e40a6fad397a88f70bbfd258df2dc2
Jeremy-michiels <19014902@student.hhs.nl>
2020-07-09 01:14:01 +0200

    .

--- src/Optionchecker/checkOptNew.java
+++ src/Optionchecker/checkOptNew.java
@@ -19,27 +19,7 @@ public class checkOptNew {
 
 
     public static Boolean optX(Field field) {
-        int[] checkOptX = new int[9];
-        int i = 0;
-        for (Buttons buttons : field.buttons) {
-            if (buttons.getTaken().equals("X")) {
-                checkOptX[i] = 1;
-            } else {
-                checkOptX[i] = 0;
-            }
-            i++;
-        }
-        boolean opt1 = checkOptX[0] == 1 && checkOptX[1] == 1 && checkOptX[2] == 1;
-        boolean opt2 = checkOptX[3] == 1 && checkOptX[4] == 1 && checkOptX[5] == 1;
-        boolean opt3 = checkOptX[6] == 1 && checkOptX[7] == 1 && checkOptX[8] == 1;
-
-        boolean opt4 = checkOptX[0] == 1 && checkOptX[3] == 1 && checkOptX[6] == 1;
-        boolean opt5 = checkOptX[1] == 1 && checkOptX[4] == 1 && checkOptX[7] == 1;
-        boolean opt6 = checkOptX[2] == 1 && checkOptX[5] == 1 && checkOptX[8] == 1;
-
-        boolean opt7 = checkOptX[0] == 1 && checkOptX[4] == 1 && checkOptX[8] == 1;
-        boolean opt8 = checkOptX[2] == 1 && checkOptX[4] == 1 && checkOptX[6] == 1;
-        if (opt1 || opt2 || opt3 || opt4 || opt5 || opt6 || opt7 || opt8) {
+        if (optCheck("X", field)) {
             return true;
         }
 
@@ -47,28 +27,35 @@ public class checkOptNew {
         return false;
     }
 
-    public static boolean optO(Field field) {
-        int[] checkOptO = new int[9];
+    private static boolean optCheck(String k, Field field){
+        int[] check = new int[9];
         int i = 0;
         for (Buttons buttons : field.buttons) {
-            if (buttons.getTaken().equals("O")) {
-                checkOptO[i] = 1;
+            if (buttons.getTaken().equals(k)) {
+                check[i] = 1;
             } else {
-                checkOptO[i] = 0;
+                check[i] = 0;
             }
             i++;
         }
-        boolean opt1 = checkOptO[0] == 1 && checkOptO[1] == 1 && checkOptO[2] == 1;
-        boolean opt2 = checkOptO[3] == 1 && checkOptO[4] == 1 && checkOptO[5] == 1;
-        boolean opt3 = checkOptO[6] == 1 && checkOptO[7] == 1 && checkOptO[8] == 1;
+        boolean opt1 = check[0] == 1 && check[1] == 1 && check[2] == 1;
+        boolean opt2 = check[3] == 1 && check[4] == 1 && check[5] == 1;
+        boolean opt3 = check[6] == 1 && check[7] == 1 && check[8] == 1;
 
-        boolean opt4 = checkOptO[0] == 1 && checkOptO[3] == 1 && checkOptO[6] == 1;
-        boolean opt5 = checkOptO[1] == 1 && checkOptO[4] == 1 && checkOptO[7] == 1;
-        boolean opt6 = checkOptO[2] == 1 && checkOptO[5] == 1 && checkOptO[8] == 1;
+        boolean opt4 = check[0] == 1 && check[3] == 1 && check[6] == 1;
+        boolean opt5 = check[1] == 1 && check[4] == 1 && check[7] == 1;
+        boolean opt6 = check[2] == 1 && check[5] == 1 && check[8] == 1;
 
-        boolean opt7 = checkOptO[0] == 1 && checkOptO[4] == 1 && checkOptO[8] == 1;
-        boolean opt8 = checkOptO[2] == 1 && checkOptO[4] == 1 && checkOptO[6] == 1;
-        if (opt1 || opt2 || opt3 || opt4 || opt5 || opt6 || opt7 || opt8) {
+        boolean opt7 = check[0] == 1 && check[4] == 1 && check[8] == 1;
+        boolean opt8 = check[2] == 1 && check[4] == 1 && check[6] == 1;
+
+
+
+        return opt1 || opt2 || opt3 || opt4 || opt5 || opt6 || opt7 || opt8;
+    }
+    public static boolean optO(Field field) {
+
+        if (optCheck("O", field)) {
             return true;
         }
 
